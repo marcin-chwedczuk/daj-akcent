ecf2a1ab36c6721ac9e63221e7a9cd339deff038
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-11-22 09:50:36 +0100

    add empty console app

---
+++ src/main/java/pl/marcinchwedczuk/Main.java
@@ -0,0 +1,12 @@
+package pl.marcinchwedczuk;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello world!");
+    }
+}
