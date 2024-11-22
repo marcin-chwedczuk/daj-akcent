e885e08d3fb28e065a6cea4773eda38e60867628
Marcin Chwedczuk <0xmarcin+gh@gmail.com>
2024-11-22 10:49:24 +0100

    reading accent dictionary

--- src/main/java/pl/marcinchwedczuk/Main.java
+++ src/main/java/pl/marcinchwedczuk/Main.java
@@ -1,15 +1,18 @@
 package pl.marcinchwedczuk;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
@@ -21,8 +24,30 @@ public class Main {
     private static final int ERRORCODE_WRONG_ARGUMENTS = 1;
     private static final int ERRORCODE_EXCEPTION = 2;
     private static final int ERRORCODE_OUTPUT_ALREADY_EXISTS = 3;
+    private static final int ERRORCODE_READING_DICTIONARY_FAILED = 4;
 
     public static void main(String[] args) {
+        Map<String, String> accentDictionary = new HashMap<>();
+        try {
+            InputStream dictionaryIS = Main.class.getResourceAsStream("/russian3-words.csv");
+            try (CSVParser csvParser = new CSVParser(new InputStreamReader(dictionaryIS, StandardCharsets.UTF_8), CSVFormat.DEFAULT)) {
+                int progress = 0;
+                for (CSVRecord record : csvParser) {
+                    progress++;
+                    if (progress % 1000 == 0) {
+                        logger.info("accent dictionary: {} records processed...", progress);
+                    }
+
+                    accentDictionary.put(record.get(2), record.get(3));
+                }
+            }
+        } catch (Exception e) {
+            logger.error("Reading dictionary failed", e);
+            System.exit(ERRORCODE_READING_DICTIONARY_FAILED);
+        }
+    }
+
+    public static void main2(String[] args) {
         args = new String[] { "/Users/mc/dev/daj-akcent/test-data/epub3-example.epub" };
 
         if (args.length != 1 || args[0] == null || args[0].isBlank()) {
